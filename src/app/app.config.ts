cad5f5d63a422fe333e142c00dcddcb032f1c306
DavidLimaturmaA <email-do-david@example.com>
2024-11-03 04:08:22 -0300

    hotfix: rollback dos arquivos iniciais

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,14 @@ import { routes } from './app.routes';
 import { provideNgxMask } from 'ngx-mask';
 
 
+// import { DishListComponent } from './components/dish-list/dish-list.component';
+// import { DishFormComponent } from './components/dish-form/dish-form.component';
+
+// const routes: Routes = [
+//   { path: '', component: DishListComponent },
+//   { path: 'add-dish', component: DishFormComponent },
+//   { path: 'edit-dish/:id', component: DishFormComponent },
+// ];
 
 export const appConfig: ApplicationConfig = {
   providers: [
