903e67746ab5c957906c795fde3af04bf15ea46f
DavidLimaturmaA <email-do-david@example.com>
2024-11-27 00:27:27 -0300

    bugfix: finalizando logica da tela de suscesso
    ps: arrumar layout do tabela de valores

--- src/app/pages/tela-pedido-sucesso/tela-pedido-sucesso.component.ts
+++ src/app/pages/tela-pedido-sucesso/tela-pedido-sucesso.component.ts
@@ -11,39 +11,32 @@ import { CommonModule } from '@angular/common';
   styleUrl: './tela-pedido-sucesso.component.css'
 })
 export class TelaPedidoSucessoComponent  implements OnInit {
-   orderDetailsMock = {
-    id: 12345,
-    status: 'PREPARANDO PEDIDO',
-    deliveryEstimate: '40 MIN',
-    address: 'João Torres Neto Miguel, 152 - Parque Sol',
-    seller: 'Trust',
-    items: [
-      {
-        name: 'Pizza de Calabresa Com Cebola Caramelizada',
-        quantity: 1,
-        price: 69.00
-      }
-    ],
-    paymentInfo: {
-      subtotal: 69.90,
-      deliveryFee: 7.00,
-      total: 76.90,
-      method: 'Pix'
-    }
-  };
 
+
+  public orderDetailsMock: any = {};
+  cep = 0
+  quantity = 0
+  total= 0;
+  taxaEntrega = 7;
   orderDetails: any;
+  item: any;
+
 
 
   constructor(private route: Router, private getPedidoSucesso: GetPedidoSucessoService){}
 
   ngOnInit(): void {
-    const orderId = 12345; // Substitua pelo ID do pedido que deseja buscar
+    const orderId = 1; // Substitua pelo ID do pedido que deseja buscar
     this.orderDetails = this.orderDetailsMock;
-
     this.getPedidoSucesso.getOrderDetails(orderId).subscribe(
       (data) => {
         this.orderDetails = data;
+        this.cep= this.orderDetails.client.cep
+        this.item = this.orderDetails.itens[0].dish;
+        this.quantity =  this.orderDetails.itens[0].quantity;
+        this.total = this.taxaEntrega + this.orderDetails.valor ;
+
+
       },
       (error) => {
         console.error('Erro ao buscar informações do pedido:', error);
